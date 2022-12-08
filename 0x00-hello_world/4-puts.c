9c372263c65da8afddca8e3fa6ccdbaddbe277a7
Error-at-night <olayemisuccess553@gmail.com>
2022-12-08 10:55:59 -0800

    4-puts.c

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -8,8 +8,8 @@
 
 int main(void)
 {
-	char *s = "\ "Programming is like building a multilingual puzzle";
-	
+	char *s = "\'Programming is like building a multilingual puzzle";
+
 	puts(s);
 
 	return (0);
