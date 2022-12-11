4b1f6f14667ea93519b805142a4ca5c4ff092494
Error-at-night <olayemisuccess553@gmail.com>
2022-12-11 09:34:59 -0800

    100-print_comb3.c

---
+++ 0x01-variables_if_else_while/100-print_comb3.c
@@ -0,0 +1,37 @@
+#include <stdio.h>
+
+/**
+ * main -Prints numbers between 00 to 89.
+ *
+ * Return: Always 0 (Success)
+*/
+int main(void)
+{
+	int z, f;
+
+	z = 48;
+	f = 48;
+
+	while (f < 58)
+	{
+		z = 48;
+		while (z < 58)
+		{
+			if (e != z && f < z)
+			{
+				putchar(f);
+				putchar(z);
+				if (z == 57 && f == 56)
+				{
+					break;
+				}
+				putchar(',');
+				putchar(' ');
+			}
+			z++
+		}
+		f++
+	}
+	putchar('\n');
+	return (0);
+}
