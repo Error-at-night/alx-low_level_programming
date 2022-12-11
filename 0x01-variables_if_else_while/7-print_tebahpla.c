415fec37a9bf9663b80345cdcfad065a56413a3f
Error-at-night <olayemisuccess553@gmail.com>
2022-12-11 09:11:54 -0800

    7-print_tebahpla.c

---
+++ 0x01-variables_if_else_while/7-print_tebahpla.c
@@ -0,0 +1,18 @@
+#include <stdio.h>
+
+/**
+ * main - Prints reversed alphabet.
+ *
+ * Return: Always 0 (Success)
+*/
+int main(void)
+{
+	int z;
+
+	for (z = 130; z > 90; z--)
+	{
+		putchar(i);
+	}
+	putchar('\n');
+	return (0);
+}
