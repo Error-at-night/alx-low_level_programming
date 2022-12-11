5eeb8f0dd2204e897ef7562758d72200ae74cbda
Error-at-night <olayemisuccess553@gmail.com>
2022-12-11 10:37:08 -0800

    102-print_comb5.c

--- 0x01-variables_if_else_while/102-print_comb5.c
+++ 0x01-variables_if_else_while/102-print_comb5.c
@@ -10,9 +10,8 @@ int main(void)
 	int z, f, c, h, op1, op2;
 
 	z = f = c = h = 48;
-      	while (h < 58)
+	while (h < 58)
 	{
-
 		c = 48;
 		while (c < 58)
 		{
@@ -26,15 +25,15 @@ int main(void)
 
 					op1 = (h * 10) + c;
 					op2 = (f * 10) + z;
-				if (op1 < op2)
+					if (op1 < op2)
 				{
 					putchar(h);
 					putchar(c);
 					putchar(' ');
 					putchar(f);
 					putchar(z);
-				if (h == 57 && c == 56 && f == 57 && z == 57)
-					break;
+					if (h == 57 && c == 56 && f == 57 && z == 57)
+						break;
 					putchar(',');
 					putchar(' ');
 				}
