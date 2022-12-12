338aca1ae8103119ce0cec53048913ba510fede4
Error-at-night <olayemisuccess553@gmail.com>
2022-12-12 03:19:14 -0800

    1-last_digit.c

--- 0x01-variables_if_else_while/1-last_digit.c
+++ 0x01-variables_if_else_while/1-last_digit.c
@@ -7,24 +7,24 @@
  *
  * Return: Always 0 (Success)
 */
-
 int main(void)
 {
-	int n = 98;
+	int n, 1;
 
 	srand(time(0));
 	n = rand() - RAND_MAX / 2;
-	if (98 > 5)
+	1 = n % 10;
+	if (1 > 5)
 	{
-		printf("Last digit of %d is %d and is greater than 5\n", n, 8);
+		printf("Last digit of %d is %d and is greater than 5\n", n, 1);
 	}
-	else if (98 == 0)
+	else if (1 == 0)
 	{
-		printf("Last digit of %d is %d and is less than 6 and not 0\n", n, 8);
+		printf("Last digit of %d is %d and is 0\n", n, 1);
 	}
 	else
 	{
-		printf("Last digit of %d is %d and is 0\n", n, 0);
+		printf("Last digit of %d is %d and is less than 6 and not 0\n", n, 1);
 	}
 	return (0);
 }
