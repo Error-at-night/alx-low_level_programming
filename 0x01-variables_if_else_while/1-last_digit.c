e7a48fac2a2027bdf60d0d69f079f19197601e86
Error-at-night <olayemisuccess553@gmail.com>
2022-12-12 03:05:46 -0800

    1-last_digit.c

--- 0x01-variables_if_else_while/1-last_digit.c
+++ 0x01-variables_if_else_while/1-last_digit.c
@@ -20,7 +20,7 @@ int main(void)
 	}
 	else if (98 == 0)
 	{
-		printf("Last digit of %d is %d and is less than 6 and not 0\n", n, -8);
+		printf("Last digit of %d is %d and is less than 6 and not 0\n", n, 8);
 	}
 	else
 	{
