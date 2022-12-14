6dded4e0341b23e60d6f5788720d5666d13df2c5
Error-at-night <olayemisuccess553@gmail.com>
2022-12-13 16:18:25 -0800

    0-putchar.c

---
+++ 0x02-functions_nested_loops/0-putchar.c
@@ -0,0 +1,38 @@
+#include "main.h"
+
+/**
+ *
+ *  * main- Print putchar
+ *
+ *   * Description: Print _putchar
+ *
+ *   * Return: 0
+ *
+*/
+
+
+
+int main(void)
+
+{
+
+	_putchar('_');
+
+	_putchar('p');
+
+	_putchar('u');
+
+	_putchar('t');
+
+	_putchar('c');
+	
+	_putchar('h');
+
+	_putchar('a');
+
+	_putchar('r');
+
+	_putchar('\n');
+
+	return (0);
+}
