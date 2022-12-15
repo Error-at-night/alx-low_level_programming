6b9faaa1e3e887c7dd1ebae58c7b0e5c316236b5
Error-at-night <olayemisuccess553@gmail.com>
2022-12-15 12:59:22 -0800

    9-times_table.c

--- 0x02-functions_nested_loops/9-times_table.c
+++ 0x02-functions_nested_loops/9-times_table.c
@@ -11,41 +11,37 @@ void times_table(void)
 
 	for (a = 0; a <= 9; a++)
 	{
-		for (a = 0; a <= 9; a++)
+		for (b = 0; b <= 9; b++)
 		{
-			for (b = 0; b <= 9; b++)
+			c = a * b;
+			if ((c / 10) == 0)
 			{
-				c = a * b;
-				if ((c / 10) == 0)
+				if (b == 0)
 				{
-					if (b == 0)
-					{
-						_putchar('0');
-					}
-					if (b != 0)
-					{
-						_putchar(' ');
-						_putchar((c % 10) + '0');
-
-					}
-					if (b < 9)
-					{
-						_putchar(',');
-						_putchar(' ');
-					}
+					_putchar('0');
 				}
-				else
+				if (b != 0)
 				{
-					_putchar((c / 10) + '0');
+					_putchar(' ');
 					_putchar((c % 10) + '0');
-					if (b < 9)
-					{
-						_putchar(',');
-						_putchar(' ');
-					}
+				}
+				if (b < 9)
+				{
+					_putchar(',');
+					_putchar(' ');
 				}
 			}
-			_putchar('\n');
+			else
+			{
+				_putchar((c / 10) + '0');
+				_putchar((c % 10) + '0');
+				if (b < 9)
+				{
+					_putchar(',');
+					_putchar(' ');
+				}
+			}
 		}
+		_putchar('\n');
 	}
 }
