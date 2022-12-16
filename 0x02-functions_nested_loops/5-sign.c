eb0e4da435bad4f66fde1eca96f518c6e67967bf
Error-at-night <olayemisuccess553@gmail.com>
2022-12-15 16:36:34 -0800

    5-sign.c

--- 0x02-functions_nested_loops/5-sign.c
+++ 0x02-functions_nested_loops/5-sign.c
@@ -1,9 +1,13 @@
 #include "main.h"
 
 /**
- * print_sign - Entry point
- * @n: Prints sign of a number
- * Return: 0 or 1
+ * print_sign - Determines if the input number
+ * greater, iqual or less than zero.
+ *
+ * @n: The input number as an integer.
+ *
+ * Return: 1 is greater than zero. 0 is zero.
+ * -1 is less than zero
 */
 int print_sign(int n)
 {
@@ -11,13 +15,18 @@ int print_sign(int n)
 
 	if (n > 0)
 	{
-		sign = 1;
-		_putchar('+');
+		_putchar(43)
+		return (1);
+	}
+	else if (n < 0)
+	{
+		_putchar(45);
+		return (-1);
 	}
 	else
 	{
-		sign = -1;
-		_putchar('-');
+		_putchar(48);
+		return (0);
 	}
-	return (sign);
+	_putchar('\n');
 }
